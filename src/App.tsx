e3b13abb7c6e86031317fbedc54b219060dad23e
xulihang <xulihanghai@163.com>
2022-01-08 17:20:31 +0800

    use table to show results

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { AppBar, Box, Button, Container, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { Button, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { useEffect, useState } from "react";
 import ButtonAppBar from "./AppBar";
 import axios from 'axios';
+import "./App.css"
 
 function App() {
   const [rows, setRows] = useState<any[]>([]);
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'image', headerName: 'Image', minWidth: 130 },
-    { field: 'result', headerName: 'Result', minWidth: 130 },
-  ];
-  const [project,setProject] = useState("");
+  const [engines, setEngines] = useState<any[]>([]);
+  const [project, setProject] = useState("");
   const [existingProjects,setExistingProjects] = useState([{"id":"1","name":"test"}]);
   const loadProjectsInfo = async () => {
     const result = await axios(
@@ -32,16 +28,52 @@ function App() {
     loadRowsFromDetails(result.data);
   };
 
+  const getImageURLFromName = (name:String) => {
+    return './projects/' + project + '/' + name;
+  }
+
+  const getEngineScoreOfImage = (engine:string, engines:any) => {
+    var score = engines[engine]["score"]; 
+    score = score.toFixed(2);
+    return score;
+  }
+
+  const loadEnginesFromImage = (image:any) => {
+    let enginesDict = image.engines;
+    var mEngines:String[] = [];
+    for (const engine in enginesDict) {
+      mEngines.push(engine);
+    }
+    setEngines(mEngines);
+  }
+
+  const getEnginesResultsOfImage = (image:any) => {
+    let enginesDict = image.engines;
+    var result = "";
+    for (const engine in enginesDict) {
+      let engineData = enginesDict[engine];
+      result = result + engineData.ocr_result;
+    }
+    return result;
+  }
+
   const loadRowsFromDetails = (details:any) => {
     var index = 0;
     var mRows = [];
+    setEngines([]);
     for (const imageName in details) {
-      let image = details[imageName];
       index = index + 1;
+      let image = details[imageName];
       var row:any = {};
       row["id"] = index;
-      row["result"] = image["ground_truth"];
       row["image"] = imageName;
+      row["ground_truth"] = image["ground_truth"];
+      row["result"] = getEnginesResultsOfImage(image);
+      row["engines"] = image["engines"];
+      //console.log(image);
+      if (index == 1){
+        loadEnginesFromImage(image);
+      }
       mRows.push(row);
     }
     setRows(mRows);
@@ -72,14 +104,38 @@ function App() {
       <Button variant="contained" onClick={showResult} color="primary">
         Show Result
       </Button>
-      <div style={{ height: 400, width: '100%' }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          checkboxSelection
-        />
-      </div>
-
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Image</th>
+            <th>Ground Truth</th>
+            <th>Result</th>
+            {engines.map(engine => (
+             <th>{engine}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row) => (
+            <tr key={row.id}>
+              <td>{row.id}</td>
+              <td>
+                <a href={getImageURLFromName(row.image)} target="_blank" >{row.image}</a>
+              </td>
+              <td>
+                {row.ground_truth}
+              </td>
+              <td>
+                {row.result}
+              </td>
+              {engines.map(engine => (
+                <th>{getEngineScoreOfImage(engine, row.engines)}</th>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
