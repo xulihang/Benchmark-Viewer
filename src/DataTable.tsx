73212ecaa4781856d3fb59acd1ccd669f6dc856e
xulihang <xulihanghai@163.com>
2022-01-10 18:09:41 +0800

    move details handlement to DataTable

--- src/DataTable.tsx
+++ src/DataTable.tsx
@@ -1,14 +1,89 @@
 import React from "react";
 import { useState } from "react";
+import axios from 'axios';
+const Diff = require("diff");
 
 export default function DataTable(props:any){
     const [rows, setRows] = useState<any[]>([]);
     const [engines, setEngines] = useState<any[]>([]);
-    React.useEffect(() => {
-        setRows(props.rows);
-        setEngines(props.engines);
-      }, [props.rows,props.engines]);
 
+    React.useEffect(() => {
+        console.log(props.project)
+        loadDetails();
+    }, [props.project]);
+    
+    const loadDetails = async ()=> {
+        if (!!props.project){
+            const result = await axios(
+            './projects/'+props.project+'/details.json',
+            );
+            loadRowsFromDetails(result.data);
+        }
+    }
+
+    const loadEnginesFromImage = (image:any) => {
+        let enginesDict = image.engines;
+        var mEngines:String[] = [];
+        for (const engine in enginesDict) {
+            mEngines.push(engine);
+        }
+        setEngines(mEngines);
+    }
+    
+    const getDiffElement = (ocr_result:string,ground_truth:string) => {
+        var container = document.createElement("div");
+        let diff = Diff.diffChars(ground_truth, ocr_result);
+        diff.forEach((part:any) => {
+            // green for additions, red for deletions
+            // grey for common parts
+            const color = part.added ? 'green' :
+            part.removed ? 'red' : 'black';
+            var span = document.createElement('span');
+            span.style.color = color;
+            span.appendChild(document
+            .createTextNode(part.value));
+            container.appendChild(span);
+        });
+        return container;
+    }
+
+
+    const getEnginesDiffOfImage = (image:any,ground_truth:string) => {
+        let enginesDict = image.engines;
+        var container = document.createElement("div");  
+        for (const engine in enginesDict) {
+            let engineData = enginesDict[engine];
+            var span = document.createElement("span");
+            span.innerText = engine+": ";
+            container.appendChild(span);
+            container.appendChild(getDiffElement(engineData["ocr_result"],ground_truth));
+        }
+        return container.outerHTML;
+    }
+
+    const loadRowsFromDetails = (details:any) => {
+        var index = 0;
+        var mRows = [];
+        setEngines([]);
+        for (const imageName in details) {
+            index = index + 1;
+            let image = details[imageName];
+            var row:any = {};
+            row["id"] = index;
+            row["image"] = imageName;
+            row["ground_truth"] = image["ground_truth"];
+            row["diffs"] = getEnginesDiffOfImage(image, image["ground_truth"]);
+            row["engines"] = image["engines"];
+            //console.log(image);
+            if (index == 1){
+            loadEnginesFromImage(image);
+            }
+            mRows.push(row);
+        }
+        setRows(mRows);
+    }
+
+    
     const getImageURLFromName = (name:String) => {
         return './projects/' + props.project + '/' + name;
     }
--- src/NavTabs.tsx
+++ src/NavTabs.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { useState } from 'react';
-import axios from 'axios';
 import DataTable from './DataTable';
 import Charts from './Charts';
-const Diff = require("diff");
+
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -44,85 +42,11 @@ function a11yProps(index: number) {
 
 export default function NavTabs(props:any) {
   const [value, setValue] = useState(0);
-  const [rows, setRows] = useState<any[]>([]);
-  const [engines, setEngines] = useState<any[]>([]);
+  const [project, setProject] = useState("");
 
   React.useEffect(() => {
-    console.log(props.project)
-    loadDetails();
+    setProject(props.project);
   }, [props.project]);
-
-  const loadDetails = async ()=> {
-    if (!!props.project){
-      const result = await axios(
-        './projects/'+props.project+'/details.json',
-      );
-      loadRowsFromDetails(result.data);
-    }
-  }
-
-  const loadEnginesFromImage = (image:any) => {
-    let enginesDict = image.engines;
-    var mEngines:String[] = [];
-    for (const engine in enginesDict) {
-      mEngines.push(engine);
-    }
-    setEngines(mEngines);
-  }
-  
-  const getDiffElement = (ocr_result:string,ground_truth:string) => {
-    var container = document.createElement("div");
-    let diff = Diff.diffChars(ground_truth, ocr_result);
-    diff.forEach((part:any) => {
-      // green for additions, red for deletions
-      // grey for common parts
-      const color = part.added ? 'green' :
-        part.removed ? 'red' : 'black';
-      var span = document.createElement('span');
-      span.style.color = color;
-      span.appendChild(document
-        .createTextNode(part.value));
-      container.appendChild(span);
-    });
-    return container;
-  }
-
-
-  const getEnginesDiffOfImage = (image:any,ground_truth:string) => {
-    let enginesDict = image.engines;
-    var container = document.createElement("div");  
-    for (const engine in enginesDict) {
-      let engineData = enginesDict[engine];
-      var span = document.createElement("span");
-      span.innerText = engine+": ";
-      container.appendChild(span);
-      container.appendChild(getDiffElement(engineData["ocr_result"],ground_truth));
-    }
-    return container.outerHTML;
-  }
-
-  const loadRowsFromDetails = (details:any) => {
-    var index = 0;
-    var mRows = [];
-    setEngines([]);
-    for (const imageName in details) {
-      index = index + 1;
-      let image = details[imageName];
-      var row:any = {};
-      row["id"] = index;
-      row["image"] = imageName;
-      row["ground_truth"] = image["ground_truth"];
-      row["diffs"] = getEnginesDiffOfImage(image, image["ground_truth"]);
-      row["engines"] = image["engines"];
-      //console.log(image);
-      if (index == 1){
-        loadEnginesFromImage(image);
-      }
-      mRows.push(row);
-    }
-    setRows(mRows);
-  }
-
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -138,7 +62,7 @@ export default function NavTabs(props:any) {
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        <DataTable engines={engines} rows={rows}/>
+        <DataTable project={project}/>
       </TabPanel>
       <TabPanel value={value} index={1}>
         <Charts/>
